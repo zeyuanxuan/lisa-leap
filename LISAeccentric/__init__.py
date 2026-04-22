daf86e7e35f271618a53b90efa1a0304c3cc5b18
Zeyuan Xuan <zeyuan.xuan.phy@gmail.com>
2026-04-21 22:46:00 -0700

    .

---
+++ LISAeccentric/__init__.py
@@ -0,0 +1,17 @@
+import sys
+import warnings
+
+# 1. 尝试导入真正的核心包
+try:
+    import leap
+except ImportError:
+    raise ImportError("Failed to find the new 'leap' package. Make sure you are in the project root.")
+
+sys.modules['LISAeccentric'] = leap
+
+warnings.warn(
+    "LISAeccentric is deprecated. Using 'leap' instead. "
+    "Please update your imports to 'import leap' to avoid this warning.",
+    DeprecationWarning,
+    stacklevel=2
+)
